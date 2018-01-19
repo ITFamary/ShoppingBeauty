3d976610340ac3f4119d3868f24541856163da8e
Ellusion <lixuefeng2012@126.com>
2018-01-19 17:44:09 +0800

    学习中..

---
+++ manage-service/src/test/java/com/ming/shopping/beauty/manage/controller/ManageMerchantControllerTest.java
@@ -0,0 +1,15 @@
+package com.ming.shopping.beauty.manage.controller;
+
+import com.ming.shopping.beauty.manage.ManageConfigTest;
+import org.junit.Test;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class ManageMerchantControllerTest extends ManageConfigTest {
+
+    @Test
+    public void go() throws Exception {
+
+        mockMvc.perform(get("/merchant")).andExpect(status().isOk());
+    }
+}
