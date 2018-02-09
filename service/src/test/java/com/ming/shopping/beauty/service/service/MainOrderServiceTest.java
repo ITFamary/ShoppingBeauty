140c07538612f6c1f2683114589d9d759876037a
Ellusion <lixuefeng2012@126.com>
2018-02-09 10:14:38 +0800

    /items 和 api同步, 查询距离我注释掉了

--- service/src/test/java/com/ming/shopping/beauty/service/service/MainOrderServiceTest.java
+++ service/src/test/java/com/ming/shopping/beauty/service/service/MainOrderServiceTest.java
@@ -3,9 +3,11 @@ package com.ming.shopping.beauty.service.service;
 import com.ming.shopping.beauty.service.CoreServiceTest;
 import com.ming.shopping.beauty.service.entity.item.Item;
 import com.ming.shopping.beauty.service.entity.item.StoreItem;
-import com.ming.shopping.beauty.service.entity.login.*;
+import com.ming.shopping.beauty.service.entity.login.Login;
+import com.ming.shopping.beauty.service.entity.login.Merchant;
+import com.ming.shopping.beauty.service.entity.login.Represent;
+import com.ming.shopping.beauty.service.entity.login.Store;
 import com.ming.shopping.beauty.service.entity.order.MainOrder;
-import com.ming.shopping.beauty.service.entity.order.OrderItem;
 import com.ming.shopping.beauty.service.model.request.OrderSearcherBody;
 import com.ming.shopping.beauty.service.repository.OrderItemRepository;
 import org.junit.Test;
@@ -13,9 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 
 import java.math.BigDecimal;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author lxf
@@ -36,9 +35,7 @@ public class MainOrderServiceTest extends CoreServiceTest {
         //生成一个订单.
         Merchant merchant = mockMerchant();
         //首先有个项目
-        Item item = itemService.addItem(merchant,null, "测试添加项目", "测试", new BigDecimal(0.01),
-                new BigDecimal(0.01), new BigDecimal(0.01), "测试添加一个项目", "这个项目用来测试", false );
-
+        Item item = mockItem(merchant);
         //生成门店项目
         Store store = mockStore(merchant);
         StoreItem storeItem = mockStoreItem(store,item);
