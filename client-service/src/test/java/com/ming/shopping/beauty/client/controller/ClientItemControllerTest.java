d9fe2da4f09d4738f90b3d5e4d7aa4da64fd46bd
Cai Jiang <luffy.ja@gmail.com>
2018-02-09 14:59:34 +0800

    合并内容

--- client-service/src/test/java/com/ming/shopping/beauty/client/controller/ClientItemControllerTest.java
+++ client-service/src/test/java/com/ming/shopping/beauty/client/controller/ClientItemControllerTest.java
@@ -1,26 +1,23 @@
 package com.ming.shopping.beauty.client.controller;
 
 import com.ming.shopping.beauty.client.ClientConfigTest;
-import com.ming.shopping.beauty.service.CoreServiceTest;
+import com.ming.shopping.beauty.service.entity.item.Item;
 import com.ming.shopping.beauty.service.entity.item.StoreItem;
+import com.ming.shopping.beauty.service.entity.item.StoreItem_;
 import com.ming.shopping.beauty.service.entity.login.Login;
+import com.ming.shopping.beauty.service.entity.login.Merchant;
+import com.ming.shopping.beauty.service.entity.login.Store;
 import com.ming.shopping.beauty.service.model.definition.ClientStoreItemModel;
-import com.ming.shopping.beauty.service.model.definition.DefinitionModel;
 import com.ming.shopping.beauty.service.repository.ItemRepository;
 import com.ming.shopping.beauty.service.repository.StoreItemRepository;
 import com.ming.shopping.beauty.service.service.StagingService;
 import me.jiangcai.lib.resource.service.ResourceService;
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.ResultMatcher;
 
-import static net.bytebuddy.implementation.FixedValue.value;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 /**
